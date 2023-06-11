2a57280e354ca59681eeacdccd5b33de51805b06
sedi <salehisediqe86@gmail.com>
2023-06-11 02:43:12 -0700

    restyled Header in a nice way

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Page.css';
+import reactLogo from "./images/react.png";
 
 
 export default function Header(){
@@ -8,8 +9,8 @@ export default function Header(){
          <header>
             <nav className="nav">
                 <div className="nav-items">
-                <img src="../images/react.jpg"
-                 alt="/" 
+                <img src={reactLogo}
+                 alt="reactlogo" 
                  className="nav-logo" />
                  <h3>ReactFacts</h3>
                 <h4>React Course-Project 1</h4> 
